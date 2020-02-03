854fd28c797d978558a6de5fdf852d0ed027fad8
Alex Lancaster <aaa633@ucsb.edu>
2020-02-03 12:49:37 -0800

    AL multiply/results fix and generic operation

--- src/main/java/edu/ucsb/cs56/ratcalc/controllers/OperationsController.java
+++ src/main/java/edu/ucsb/cs56/ratcalc/controllers/OperationsController.java
@@ -55,14 +55,16 @@ public class OperationsController {
         return false;
     }
 
-    public void doGenericOperation(@Valid RatCalcForm ratCalcForm, BindingResult bindingResult, GenericRationalOp opFunction) {
+    public void doGenericOperation(@Valid RatCalcForm ratCalcForm, BindingResult bindingResult, GenericRationalOp opFunction, boolean checkSecondTopNonzero) {
         if (!bindingResult.hasErrors() && !checkDenominatorErrors(ratCalcForm)) {
-            Rational r1 = new Rational(ratCalcForm.getNum1(), ratCalcForm.getDenom1());
-            Rational r2 = new Rational(ratCalcForm.getNum2(), ratCalcForm.getDenom2());
-            Rational result = opFunction.apply(r1, r2);
-            logger.info("r1=" + r1 + " r2=" + r2 + " result=" + result);
-            ratCalcForm.setNumResult(result.getNumerator());
-            ratCalcForm.setDenomResult(result.getDenominator());
+            if ((!checkSecondTopNonzero || !checkDivideByZero(ratCalcForm))) {
+                Rational r1 = new Rational(ratCalcForm.getNum1(), ratCalcForm.getDenom1());
+                Rational r2 = new Rational(ratCalcForm.getNum2(), ratCalcForm.getDenom2());
+                Rational result = opFunction.apply(r1, r2);
+                logger.info("r1=" + r1 + " r2=" + r2 + " result=" + result);
+                ratCalcForm.setNumResult(result.getNumerator());
+                ratCalcForm.setDenomResult(result.getDenominator());
+            }
         }
     }
 
@@ -78,7 +80,7 @@ public class OperationsController {
     public String getAddResult(Model model, @Valid RatCalcForm ratCalcForm, BindingResult bindingResult) {
         logger.info("getAddResult ratCalcForm=" + ratCalcForm);
         ratCalcForm.setOp("+");
-        doGenericOperation(ratCalcForm, bindingResult, (x,y) -> Rational.sum(x,y));
+        doGenericOperation(ratCalcForm, bindingResult, (x,y) -> Rational.sum(x,y), false);
         model.addAttribute("ratCalcForm", ratCalcForm);
         return "operations/add";
     }
@@ -95,7 +97,7 @@ public class OperationsController {
     public String getSubtractResult(Model model, @Valid RatCalcForm ratCalcForm, BindingResult bindingResult) {
         logger.info("getSubtractResult ratCalcForm=" + ratCalcForm);
         ratCalcForm.setOp("-");
-        doGenericOperation(ratCalcForm, bindingResult, (x,y) -> Rational.difference(x,y));
+        doGenericOperation(ratCalcForm, bindingResult, (x,y) -> Rational.difference(x,y), false);
         model.addAttribute("ratCalcForm", ratCalcForm);
         return "operations/subtract";
     }
@@ -103,7 +105,7 @@ public class OperationsController {
     @GetMapping("/multiply")
     public String getMultiply(Model model) {
         RatCalcForm ratCalcForm = new RatCalcForm();
-        ratCalcForm.setOp("x");
+        ratCalcForm.setOp("×");
         model.addAttribute("ratCalcForm", ratCalcForm);
         return "operations/multiply";
     }
@@ -111,10 +113,27 @@ public class OperationsController {
     @GetMapping("/multiply/results")
     public String getMultiplyResult(Model model, @Valid RatCalcForm ratCalcForm, BindingResult bindingResult) {
         logger.info("getMultiplyResult ratCalcForm=" + ratCalcForm);
-        ratCalcForm.setOp("-");
-        doGenericOperation(ratCalcForm, bindingResult, (x,y) -> Rational.product(x,y));
+        ratCalcForm.setOp("×");
+        doGenericOperation(ratCalcForm, bindingResult, (x,y) -> Rational.product(x,y), false);
         model.addAttribute("ratCalcForm", ratCalcForm);
         return "operations/multiply";
+    }
+
+    @GetMapping("/divide")
+    public String getDivide(Model model) {
+        RatCalcForm ratCalcForm = new RatCalcForm();
+        ratCalcForm.setOp("÷");
+        model.addAttribute("ratCalcForm", ratCalcForm);
+        return "operations/divide";
+    }
+
+    @GetMapping("/divide/results")
+    public String getMultiplyResult(Model model, @Valid RatCalcForm ratCalcForm, BindingResult bindingResult) {
+        logger.info("getDivideResult ratCalcForm=" + ratCalcForm);
+        ratCalcForm.setOp("÷");
+        doGenericOperation(ratCalcForm, bindingResult, (x,y) -> Rational.quotient(x,y), true);
+        model.addAttribute("ratCalcForm", ratCalcForm);
+        return "operations/divide";
     }
 
 }
